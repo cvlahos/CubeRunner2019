2b88db72206f511d75447186b85c7e69281b11c6
cvlahos <cvlahos@users.noreply.github.com>
2016-11-28 08:47:46 -0500

    Add files via upload

--- BadGuyJump.cs
+++ BadGuyJump.cs
@@ -4,31 +4,33 @@ using System.Collections;
 public class BadGuyJump : MonoBehaviour 
 {
 
+	// for enemy AI;
 	public Rigidbody2D badGuyRb;
-
 	public float badGuyJumpSpeed;
 	public float timeBefore1stJump;
 	public float timeBeforeNextJump;
+	
 
-	public SpriteRenderer badGuySpriteRenderer;
-
+	//need to access the level manager to reset the leve when the player is beat
 	public LevelManager levelManager;
 
+	// if the player lands on the enemy then the player will destroy the enemy and get a boost up in the air
 	public Rigidbody2D playerRB;
 	public float pushUpForce;
 
 	public MusicManager musicManager;
-	//float timer;
 
+
+	//don't worry about the camera
 	public CamCtrl camCtrl;
 
-	//public float switchDirection;
+
 
 	// Use this for initialization
 	void Start () 
 	{
 		InvokeRepeating ("badGuyJumpAttack",timeBefore1stJump,timeBeforeNextJump);
-		//timer = badGuyJumpDuration;
+
 	}
 	
 	// Update is called once per frame
@@ -41,10 +43,11 @@ public class BadGuyJump : MonoBehaviour
 
 	void badGuyJumpAttack()
 	{
-		
+
 		badGuyRb.AddForce(new Vector2 (0, badGuyJumpSpeed));
 	}
 
+	// when there is a collision with any other part of the player bad stuff will happen
 	void OnCollisionEnter2D (Collision2D enemyCollision)
 	{
 		if (enemyCollision.gameObject.tag == "Player") 
@@ -55,6 +58,8 @@ public class BadGuyJump : MonoBehaviour
 	
 	}
 
+
+	// when there is a trigger collision with the bottom of the player the enemy will be destroyed and the player will get points and a boost up
 	void OnTriggerEnter2D (Collider2D enemyTrigCollision)
 	{
 		if (enemyTrigCollision.gameObject.tag == "Player") 
@@ -65,30 +70,10 @@ public class BadGuyJump : MonoBehaviour
 			musicManager.PlayEnemyDestroyedSound();
 			playerRB.AddForce(new Vector2 (0,pushUpForce));
 			Destroy(gameObject);
+		
 		} 
 		
 	}
-	//if you want the bad guy to flip sprite
-	/*
-	void OnTriggerExit2D (Collider2D enemyCollision)
-	{
-		if (enemyCollision.gameObject.tag == "Platform") 
-		{
-			
-			badGuySpriteRenderer.flipY = true;
-		} 
 
-	}
-
-	void OnTriggerStay2D (Collider2D enemyCollision)
-	{
-		if (enemyCollision.gameObject.tag == "Platform") 
-		{
-
-			badGuySpriteRenderer.flipY = false;
-		} 
-
-	}
-*/
 
 }
