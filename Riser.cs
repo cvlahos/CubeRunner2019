7c8fa3aa80e9184140d93e75d5aa0e4f50aafdf4
cvlahos <cvlahos@users.noreply.github.com>
2016-11-29 14:56:25 -0500

    Add files via upload

---
+++ Riser.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Riser : MonoBehaviour 
+{
+	// take control of the platform's rigid body
+	public Rigidbody2D platformRB;
+	
+	// need a variable for how long the platform should stay before falling
+	public float riseDelay;
+
+	public float riseSpeed;
+	public float transportSpeed;
+
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	
+	void OnCollisionStay2D(Collision2D playerCollision)
+	{
+		if (playerCollision.gameObject.tag == "Player") 
+		{
+			//Invoke the platform rise function
+			Invoke("PlatformMove", riseDelay);
+
+			
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D playerCollisionExit)
+	{
+		if (playerCollisionExit.gameObject.tag == "Player") 
+		{
+
+			platformRB.velocity = new Vector2 (0f, 0f);
+			CancelInvoke();
+			
+		}
+	}
+	
+	void PlatformMove()
+	{
+
+		platformRB.velocity = new Vector2 (transportSpeed, riseSpeed);
+	}
+}
+
